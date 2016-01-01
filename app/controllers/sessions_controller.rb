864b873a8e122473bbbc4c19ca64829b69b02ebb
Michelle Jolliffe <michelle_jolliffe@alumni.brown.edu>
2016-01-01 12:59:09 -0800

    Login/Logout/Remember me basic functionality. Still need to style.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      #if remember me is clicked, remember the user, else forget the user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
+
   def destroy
+    log_out if logged_in?
+    redirect_to root_path
   end
 end
