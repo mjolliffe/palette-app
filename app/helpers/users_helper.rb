37bdac18ad90d5ca8e3ff4d9f250e9e633f6d373
Michelle Jolliffe <michelle_jolliffe@alumni.brown.edu>
2016-01-07 20:26:19 -0800

    Working on changing gravatar default.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,6 +4,7 @@ module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_default = "http://www.gravatar.com/avatar/?d=https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Paint_palette_icon_from_the_Noun_Project.svg/100px-Paint_palette_icon_from_the_Noun_Project.svg.png"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,3 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 #Seed courses
-courses = Course.create([{
-name: "Beginning Modern Calligraphy",
-instructor: "Michelle Jolliffe",
-street_address: "9355 Airway Rd. #4",
-zip_code: 92154,
-course_image_url: "http://juliabausenhardt.com/wp-content/uploads/2015/07/01-beginners-guide-to-calligraphy1.jpg",
-sessions_per_course: 1,
-start_date: "2016-12-12",
-end_date: "2016-12-12",
-course_day_of_week: "Monday",
-description: "Learn the basics of modern calligraphy with a local expert. Ink, paper, and quills provided!",
-price: "90.0",
-creator_id: 1
-}])
