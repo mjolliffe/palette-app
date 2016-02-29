a08f502f6d94f3aeed8e3d6e264730413b8356fa
Michelle Jolliffe <michelle_jolliffe@alumni.brown.edu>
2016-02-29 15:00:17 -0800

    Reseeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-courses = Course.create([{
-  name:"B+W Photography",
-  instructor:"Lee Saper",
-  street_address:"811 W 7th St",
-  zip_code: "90017",
-  course_image_url: "http://webneel.com/daily/sites/default/files/images/daily/04-2013/21-wet-corridor-by-rui-palha-bw-photography.jpg",
-  sessions_per_course: "1",
-  start_date: "04/22/2016",
-  end_date: "04/22/2016",
-  course_day_of_week: "Friday",
-  start_time: "6 PM",
-  end_time: "9 PM",
-  description: "Learn the basics of black and white photography.",
-  price: 250,
-  creator_id: 1
-  },
-  {
-  name:"Ceramics 101",
-  instructor:"Melissa Dichter",
-  street_address:"2411 Glendale Blvd.",
-  zip_code: "90039",
-  course_image_url: "http://www.creswell.k12.or.us/wp/stepp/files/2015/09/clay11.jpg",
-  sessions_per_course: "1",
-  start_date: "04/20/2016",
-  end_date: "04/20/2016",
-  course_day_of_week: "Wednesday",
-  start_time: "12 PM",
-  end_time: "5 PM",
-  description: "Learn the basics of sculpting and firing clay.",
-  price: 250,
-  creator_id: 2
-  }])
 
 users = User.create([{
   name: "Michelle Jolliffe",
